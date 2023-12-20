ae000a50a3351f7df378433ae0cdc1001c5193f6
harshita sethiya <harshita.sethiya252@gmail.com>
2023-12-20 13:25:05 +0530

    demo

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-const http=require('http');
+// const http=require('http');
  
-const server=http.createServer((req,res)=>{
-    console.log(Harshitasethiya);
+// const server=http.createServer((req,res)=>{
+//     console.log(Harshitasethiya);
+// });
+// server.listen(4000);
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 4000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, this is the server!\n');
 });
-server.listen(4000);
+
+server.listen(port, hostname, () => {
+  console.log('Server running at http://' + hostname + ':' + port + '/');
+  console.log('My name is Harshita sethiya.');
+});
